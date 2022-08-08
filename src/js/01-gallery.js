f1713f4e2b65e8886c7af162ef3a665f6c3e7561
Daniel Prandzioch <prandziochdaniel32@gmail.com>
2022-08-08 19:17:07 +0200

    errory w konsoli

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,26 +1,33 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 // Opisany w dokumentacji
-import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from './simplelightbox';
 // Dodatkowy import stylów
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import './simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
-const markup = galleryItems
+
+const galleryItem = galleryItems
   .map(
-    image =>
-      `<a class="gallery__item" href="${image.original}"> <img class= "gallery__image" src="${image.preview}" 
- alt="${image.description}"></a>`
+    galleryItem => `<div class="gallery__item">
+<a class="gallery__item" href="${galleryItem.original}">
+  <img
+    class="gallery__image"
+    src="${galleryItem.preview}"
+    alt="${galleryItem.description}"
+  />
+</a>
+</div>`
   )
   .join('');
-gallery.insertAdjacentHTML('beforeend', markup);
+
+gallery.insertAdjacentHTML('beforeend', galleryItem);
 
 console.log(galleryItems);
 
-
-let lightbox = new SimpleLightbox('.gallery a', {
+var lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
   captionsData: 'alt',
 });
