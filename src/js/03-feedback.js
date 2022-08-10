6f50f4b70feba31a57c3f463958cdb72c2245433
Daniel Prandzioch <prandziochdaniel32@gmail.com>
2022-08-10 17:22:40 +0200

    zadanie 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(localNote, 500));
+
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+function localNote() {
+  const formNote = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formNote));
+}
+
+function getLocalNote() {
+  let localNote = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (localNote !== null) {
+    email.value = localNote.email;
+    message.value = localNote.message;
+  }
+}
+
+getLocalNote();
+
+form.addEventListener('submit', submitNote);
+
+function submitNote(e) {
+  e.preventDefault();
+  this.reset();
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state');
+}
